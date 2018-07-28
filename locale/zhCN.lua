8db3cd106e4ac9814ea988b80ab32c03b15ace3f
EK <eke00372@gmail.com>
2018-07-28 18:47:12 +0800

    Update zhCN.lua (#32)
    * Update zhCN.lua
    
    * Update zhCN.lua

--- locale/zhCN.lua
+++ locale/zhCN.lua
@@ -4,18 +4,18 @@ local L = select(2, ...)('zhCN')
 L['The Collapse'] = '大陷坑'
 
 -- Config
--- L['Automatically share quests when picked up'] = '' -- MISSING!
--- L['Automatically select single gossip options'] = '' -- MISSING!
--- L['When to select gossip while in a raid'] = '' -- MISSING!
--- L['Automatically pay Darkmoon Faire teleporting fees'] = '' -- MISSING!
--- L['Reverse the behaviour of the modifier key'] = '' -- MISSING!
--- L['Hold this key to to temporarily enable automation'] = '' -- MISSING!
--- L['Hold this key to to temporarily disable automation'] = '' -- MISSING!
--- L['Disable while doing the withered training scenario in Suramar'] = '' -- MISSING!
--- L['Always accept and complete Nomi\'s daily quest, despite being low-level'] = '' -- MISSING!
--- L['Quests starting with and/or containing these items will not be automated.'] = '' -- MISSING!
--- L['Drag items into the window below to add more.'] = '' -- MISSING!
--- L['Right-click to remove item'] = '' -- MISSING!
+L['Automatically share quests when picked up'] = '拾取任务物品后自动接任务'
+L['Automatically select single gossip options'] = '自动选择单一对话选项'
+L['When to select gossip while in a raid'] = '当你在团队中，是否启用自动选择对话选项的功能'
+L['Automatically pay Darkmoon Faire teleporting fees'] = '自动支付暗月马戏团传送费'
+L['Reverse the behaviour of the modifier key'] = '反转辅助键的行为（勾选后，按下按键才启用自动交接）'
+L['Hold this key to to temporarily enable automation'] = '按住这个按键可以暂时启用自动交接'
+L['Hold this key to to temporarily disable automation'] = '按住这个按键可以暂时禁用自动交接'
+L['Disable while doing the withered training scenario in Suramar'] = '位于苏拉玛尔的枯法者训练场时禁用'
+L['Always accept and complete Nomi\'s daily quest, despite being low-level'] = '总是接受并完成糯米的每日任务，尽管是低级别'
+L['Quests starting with and/or containing these items will not be automated.'] = '以这些物品为起始的任务将不会自动接受'
+L['Drag items into the window below to add more.'] = '增加内容清单：将物品拖曳至下面的视窗内'
+L['Right-click to remove item'] = '右键点击将物品从列表移除'
 
 L['ALT key'] = ALT_KEY
 L['CTRL key'] = CTRL_KEY
@@ -23,4 +23,4 @@ L['SHIFT key'] = SHIFT_KEY
 
 L['Never'] = NEVER
 L['Always'] = ALWAYS
--- L['Soloing'] = '' -- MISSING!
+L['Soloing'] = '单人'
