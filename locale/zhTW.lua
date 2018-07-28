06cf43c855289424c5c2b25ec4e48e08ab25aad2
EK <eke00372@gmail.com>
2018-07-28 16:59:04 +0800

    Update zhTW.lua

--- locale/zhTW.lua
+++ locale/zhTW.lua
@@ -4,18 +4,18 @@ local L = select(2, ...)('zhTW')
 L['The Collapse'] = '崩塌遺跡'
 
 -- Config
--- L['Automatically share quests when picked up'] = '' -- MISSING!
--- L['Automatically select single gossip options'] = '' -- MISSING!
--- L['When to select gossip while in a raid'] = '' -- MISSING!
--- L['Automatically pay Darkmoon Faire teleporting fees'] = '' -- MISSING!
--- L['Reverse the behaviour of the modifier key'] = '' -- MISSING!
--- L['Hold this key to to temporarily enable automation'] = '' -- MISSING!
--- L['Hold this key to to temporarily disable automation'] = '' -- MISSING!
--- L['Disable while doing the withered training scenario in Suramar'] = '' -- MISSING!
--- L['Always accept and complete Nomi\'s daily quest, despite being low-level'] = '' -- MISSING!
--- L['Quests starting with and/or containing these items will not be automated.'] = '' -- MISSING!
--- L['Drag items into the window below to add more.'] = '' -- MISSING!
--- L['Right-click to remove item'] = '' -- MISSING!
+L['Automatically share quests when picked up'] = '拾取任務物品後自動接任務'
+L['Automatically select single gossip options'] = '自動選擇單一對話選項'
+L['When to select gossip while in a raid'] = '當你在團隊中，啟用自動選擇對話選項的功能'
+L['Automatically pay Darkmoon Faire teleporting fees'] = '自動支付暗月馬戲團傳送費'
+L['Reverse the behaviour of the modifier key'] = '反轉輔助鍵的行為（勾選後，按下按鍵才啟用自動交接）'
+L['Hold this key to to temporarily enable automation'] = '按住這個按鍵可以暫時啟用自動交接'
+L['Hold this key to to temporarily disable automation'] = '按住這個按鍵可以暫時禁用自動交接'
+L['Disable while doing the withered training scenario in Suramar'] = '位於蘇拉瑪爾的凋萎者訓練場時禁用'
+L['Always accept and complete Nomi\'s daily quest, despite being low-level'] = '總是接受並完成糯米的每日任務，儘管是低級別'
+L['Quests starting with and/or containing these items will not be automated.'] = '以這些物品為起始的任務將不會自動接受'
+L['Drag items into the window below to add more.'] = '增加內容清單：將物品拖曳至下面的視窗內'
+L['Right-click to remove item'] = '右鍵點擊將物品從列表移除'
 
 L['ALT key'] = ALT_KEY
 L['CTRL key'] = CTRL_KEY
@@ -23,4 +23,4 @@ L['SHIFT key'] = SHIFT_KEY
 
 L['Never'] = NEVER
 L['Always'] = ALWAYS
-L['Soloing'] = '獨自'
+L['Soloing'] = '單人'
