cc5b5123da5d936b6ed2cfb60bcd0a73df88a4a3
Adrian L Lange <mail@p3lim.net>
2020-08-08 23:25:21 +0200

    Drop the symlinks, filter lua errors instead

--- utils/link.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/.."
-
-[[ ! -d libs ]] && mkdir -p libs
-[[ ! -L libs/LibStub ]] && ln -s ../LibStub libs/LibStub
-[[ ! -L libs/Wasabi ]] && ln -s ../Wasabi libs/Wasabi
