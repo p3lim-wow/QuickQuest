cdd16fd06e67efe1f02209422317c6a0f88f4bd8
Adrian L Lange <mail@p3lim.net>
2020-08-09 17:46:16 +0200

    Implement options using AceGUI/AceConfig
    Because I'm lazy and want less stuff to maintain

--- Options.lua
+++ Options.lua
@@ -144,13 +144,86 @@ ns.EventHandler:Register('ADDON_LOADED', function(...)
 				ns.db.profile.general.pausekeyreverse = QuickQuestDB.reverse
 			end
 
+			-- TODO: empty old db:
 			-- QuickQuestDB = nil
 		end
 		if(QuickQuestBlacklistDB and QuickQuestBlacklistDB.items) then
 			ns.db.profile.blocklist.items = QuickQuestBlacklistDB.items
+			-- TODO: empty old db:
 			-- QuickQuestBlacklistDB = nil
 		end
 
 		return true -- unregister
 	end
+end)
+
+local function CreateOptions()
+	CreateOptions = nop -- we only want to load this once
+
+	LibStub('AceConfig-3.0'):RegisterOptionsTable(addonName, {
+		type = 'group',
+		get = function(info)
+			return ns.db.profile.general[info[#info]]
+		end,
+		set = function(info, value)
+			ns.db.profile.general[info[#info]] = value
+		end,
+		args = {
+			share = {
+				order = 1,
+				name = L['Automatically share quests when picked up'],
+				type = 'toggle',
+				width = 'double',
+			},
+			skipgossip = {
+				order = 2,
+				name = L['Automatically select single gossip options'],
+				type = 'toggle',
+				width = 'double',
+			},
+			skipgossipwhen = {
+				order = 3,
+				name = L['When to select gossip while in a raid'],
+				type = 'select',
+				width = 'double',
+				values = {
+					[0] = L['Never'],
+					[1] = L['Soloing'],
+					[2] = L['Always']
+				}
+			},
+			paydarkmoonfaire = {
+				order = 4,
+				name = L['Automatically pay Darkmoon Faire teleporting fees'],
+				type = 'toggle',
+				width = 'double',
+			},
+			pausekey = {
+				order = 5,
+				name = L['Hold this key to to temporarily pause automation'],
+				type = 'select',
+				width = 'double',
+				values = {
+					ALT = L['ALT key'],
+					CTRL = L['CTRL key'],
+					SHIFT = L['SHIFT key']
+				}
+			},
+			pausekeyreverse = {
+				order = 6,
+				name = L['Reverse the behaviour of the modifier key'],
+				type = 'toggle',
+				width = 'double',
+			},
+		},
+	})
+
+	LibStub('AceConfigDialog-3.0'):AddToBlizOptions(addonName)
+end
+
+InterfaceOptionsFrameAddOns:HookScript('OnShow', function()
+	CreateOptions() -- LoD
+
+	-- we load too late, so we have to manually refresh the list
+	InterfaceAddOnsList_Update()
 end)
