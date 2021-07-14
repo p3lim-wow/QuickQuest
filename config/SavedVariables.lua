35b06831c8a8ed414fbeb5289dbc9da0b93f2a38
Adrian L Lange <mail@p3lim.net>
2021-07-14 15:24:02 +0200

    Ignore Ve'nari repeatable

--- config/SavedVariables.lua
+++ config/SavedVariables.lua
@@ -145,6 +145,9 @@ local defaults = {
 				[54462] = true, -- Supplies from Zandalari Empire
 				[55348] = true, -- Supplies from the Rustbolt Resistance
 				[55976] = true, -- Supplies From the Waveblade Ankoan
+
+				-- 9.0 valuable resources
+				[64541] = true, -- The Cost of Death (Ve'nari)
 			},
 		},
 	},
