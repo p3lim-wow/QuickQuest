99e03fdf7b8dfe7aa93d3d8ff68ecc762536f0c7
BlueNightSky <afan1128@hotmail.com>
2023-04-06 07:14:49 +0800

    Update zhTW

--- locale/zhTW.lua
+++ locale/zhTW.lua
@@ -2,28 +2,28 @@ if GetLocale() ~= 'zhTW' then return end
 local L = select(2, ...).L('zhTW')
 
 -- Config
--- L['Automatically accept quests'] = '' -- MISSING!
--- L['Automatically complete quests'] = '' -- MISSING!
+L['Automatically accept quests'] = '自動接受任務'
+L['Automatically complete quests'] = '自動完成任務'
 L['Automatically share quests when picked up'] = '接受任務後自動分享'
 L['Automatically select single gossip options'] = '自動選擇單一對話選項'
 L['When to select gossip while in a raid'] = '當你在團隊中，是否啟用自動選擇對話選項的功能'
 L['Automatically pay Darkmoon Faire teleporting fees'] = '自動支付暗月馬戲團傳送費'
 L['Reverse the behaviour of the modifier key'] = '反轉輔助鍵的行為（勾選後，按下按鍵才啟用自動交接）'
--- L['Hold this key to to temporarily pause automation'] = '' -- MISSING!
--- L['Automatically select the reward that\'s worth the most'] = '' -- MISSING!
--- L['Automatically deliver repeatable delivery quests'] = '' -- MISSING!
+L['Hold this key to to temporarily pause automation'] = '按下此按鍵來暫時跳過自動化'
+L['Automatically select the reward that\'s worth the most']  = '自動選擇比較值錢的獎勵'
+L['Automatically deliver repeatable delivery quests'] = '自動提交重複的每日任務' 
 
--- L['Item Blocklist'] = '' -- MISSING!
--- L['NPC Blocklist'] = '' -- MISSING!
--- L['Title Blocklist'] = '' -- MISSING
--- L['Block Item'] = '' -- MISSING!
--- L['Block NPC'] = '' -- MISSING!
--- L['Block Title']) = '' --MISSING!
--- L['Quests containing items in this list will not be automated.'] = '' -- MISSING!
--- L['Quests and dialogue from NPCs in this list will not be automated.'] = '' -- MISSING!
+L['Item Blocklist'] = '物品阻擋清單'
+L['NPC Blocklist'] = 'NPC阻擋清單'
+L['Title Blocklist'] = '抬頭阻擋清單'
+L['Block Item'] = '阻擋物品'
+L['Block NPC'] = '阻擋NPC'
+L['Block Title'] = '阻擋抬頭'
+L['Quests containing items in this list will not be automated.'] = '在此清單中包含物品的任務將不會自動化。'
+L['Quests and dialogue from NPCs in this list will not be automated.'] = '在此清單中來自NPC的對話與任務將不會自動化。'
 
--- L['Block a new item by ID'] = '' -- MISSING!
--- L['Block a new NPC by ID'] = '' -- MISSING!
+L['Block a new item by ID'] = '阻擋新的物品根據ID'
+L['Block a new NPC by ID'] = '阻擋新的NPC根據ID'
 -- L['Quests (by partial title or ID) in this list will not be automated.'] = '' -- MISSING!
 
 L['ALT key'] = ALT_KEY
